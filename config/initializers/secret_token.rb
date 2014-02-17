d06266c4be10f7e493b9d93d9bc93ea8e1ae4360
jarzayus <jarzayus@gmail.com>
2014-02-17 02:53:55 -0500

    new test repo
    cms v2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCmsTwo::Application.config.secret_key_base = '7c387084693715acee15be4595657d1313e7de5b87f8adae8d5dc3f9cb8196a15bf84d219a039a367e8dd32da5a2ffed79a6e33ef72c650996bd0867792d49a0'
